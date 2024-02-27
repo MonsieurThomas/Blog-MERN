f0051665271969b1bf53f8874f5c29a4d127cac0
Thomas Romain <r.thomas06200@gmail.con>
2024-02-27 14:22:24 +0100

    first

---
+++ client/src/components/Auth.js
@@ -0,0 +1,139 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const Auth = () => {
+  const naviagte = useNavigate();
+  const dispath = useDispatch();
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const location = useLocation();
+  const [isSignup, setIsSignup] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+
+  useEffect(() =>{
+    const initialIsSignup  = location.state?.initialIsSignup ?? false;
+    setIsSignup(initialIsSignup);
+  }, [location.state]);
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const sendRequest = async (type = "login") => {
+    try {
+      const res = await axios.post(`http://localhost:5000/api/user/${type}`, {
+        name: inputs.name,
+        email: inputs.email,
+        password: inputs.password,
+      });
+  
+      const data = await res.data;
+      console.log(data);
+      return data;
+    } catch (err) {
+      setErrorMessage(err.response?.data?.message || "An error occurred");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    if (isSignup) {
+      sendRequest("signup")
+      .then((data) => {
+        if (data) {
+          localStorage.setItem("userId", data.user._id)
+          dispath(authActions.login())
+          naviagte("/blogs");
+        }
+      })
+    } else {
+      sendRequest()
+        .then((data) => {
+          if (data) {
+            localStorage.setItem("userId", data.user._id)
+            .then(() => dispath(authActions.login()))
+            .then(() => naviagte("/blogs"));
+          }
+        })
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          maxWidth={400}
+          display="flex"
+          flexDirection={"column"}
+          alignItems="center"
+          justifyContent={"center"}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={5}
+          borderRadius={5}
+        >
+          <Typography variant="h2" padding={3} textAlign="center">
+            {isSignup ? "Signup" : "Login"}
+          </Typography>
+          {isSignup && (
+            <TextField
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+              placeholder="Name"
+              margin="normal"
+            />
+          )}{" "}
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            type={"password"}
+            placeholder="Password"
+            margin="normal"
+          />
+          <Typography color="error" textAlign="center">
+            {errorMessage}
+          </Typography>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignup(!isSignup)}
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            Change To {isSignup ? "Login" : "Signup"}
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
